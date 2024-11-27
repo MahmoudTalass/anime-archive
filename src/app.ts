e6ef0e86d4befd753072a70a70b9ee164aa59d7d
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-27 15:25:43 -0500

    Add catch all error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express, { Express } from "express";
+import express, { Request, Response, Express, NextFunction } from "express";
 import indexRouter from "./indexRoute";
 import dotenv from "dotenv";
+import { errorHandlingMiddleware } from "./helpers/errorHelpers";
 
 dotenv.config({ path: __dirname + "/../.env" });
 const app: Express = express();
 
 app.use("/api/v1", indexRouter);
+
+app.use(errorHandlingMiddleware);
 
 const PORT: number = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/helpers/errorHelpers.ts
+++ src/helpers/errorHelpers.ts
@@ -1,3 +1,5 @@
+import { Response, Request, NextFunction } from "express";
+
 export class AppError extends Error {
     public statusCode: number;
 
@@ -9,4 +11,19 @@ export class AppError extends Error {
 
 export function logError(statusCode: number, errorMessage: string) {
     console.error(`status: ${statusCode}, message: ${errorMessage}`);
+}
+
+export function errorHandlingMiddleware(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void {
+    let statusCode: number = error instanceof AppError ? error.statusCode : 500;
+    const message: string = error.message || "Something went wrong.";
+
+    res.status(statusCode).json({
+        statusCode,
+        message,
+    });
 }
