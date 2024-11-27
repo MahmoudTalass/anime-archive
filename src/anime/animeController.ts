fe25828dfcbfff66bdf08928980b4b883d83867c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-26 22:45:19 -0500

    Add error handling to getAnimes and implement getAnime
    getAnime controller function retreives a single anime by their id number and if they are not found, 404 status code is returned

--- src/anime/animeController.ts
+++ src/anime/animeController.ts
@@ -1,30 +1,60 @@
-import { default as asyncHanlder } from "express-async-handler";
+import { default as asyncHandler } from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
-import { IAnime } from "./animeTypes";
+import { AnimeApiResponse, IAnime } from "./animeTypes";
+import { AppError, logError } from "../helpers/errorHelpers";
 
-const getAnimes = asyncHanlder(async (req: Request, res: Response, next: NextFunction) => {
+function transformAnimeData(anime: Record<string, any>): IAnime {
+    return {
+        malId: anime.mal_id,
+        title: anime.title_english ?? anime.title,
+        url: anime.url,
+        imageUrl: anime.images.jpg.image_url,
+        episodes: anime.episodes,
+        synopsis: anime.synopsis,
+        genres: anime.genres.map((genre: Record<string, any>) => genre.name),
+        year: anime.year,
+    };
+}
+
+const getAnimes = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     // used || since NaN would pass ?? operator
     const pageNumber = Number(req.query.page) || 1;
-    const searchTerm = req.query.page ?? req.query.q;
+    const searchTerm = req.query.q ?? "";
 
     const response = await fetch(
         `${process.env.ANIME_API_BASE_URL}/anime?page=${pageNumber}&q=${searchTerm}`
     );
-    const { data, pagination } = await response.json();
+
+    if (!response.ok) {
+        logError(response.status, response.statusText);
+        throw new AppError("Could not retreive anime data, please try again later.", 502);
+    }
+
+    const { data, pagination } = (await response.json()) as AnimeApiResponse;
 
     const animeData: IAnime[] = data.map((anime: Record<string, any>) => {
-        return {
-            malId: anime.mal_id,
-            title: anime.title_english,
-            url: anime.url,
-            imageUrl: anime.images.jpg.image_url,
-            episodes: anime.episodes,
-            synopsis: anime.synopsis,
-            genres: anime.genres.map((genre: Record<string, any>) => genre.name),
-        };
+        return transformAnimeData(anime);
     });
 
     res.json({ pagination, animeData });
 });
 
-export { getAnimes };
+const getAnime = asyncHandler(async (req: Request, res: Response) => {
+    const { malId } = req.params;
+    const response = await fetch(`${process.env.ANIME_API_BASE_URL}/anime/${malId}`);
+
+    if (!response.ok) {
+        logError(response.status, response.statusText);
+        if (response.status === 404) {
+            throw new AppError("Anime not found.", 404);
+        }
+        throw new AppError("Could not retreive anime data.");
+    }
+
+    const { data } = (await response.json()) as AnimeApiResponse;
+    const anime: IAnime = transformAnimeData(data);
+
+    res.json({ anime });
+});
+
+export { getAnimes, getAnime };
