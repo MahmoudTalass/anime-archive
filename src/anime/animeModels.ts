1545bd0478e4c7b832edbaace1c6d87546c039a6
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-26 22:28:35 -0500

    Replace rating field with score
    It is more accurate for representing how the user scores the anime rather than being confused with anime rating (eg. PG13)

--- src/anime/animeModels.ts
+++ src/anime/animeModels.ts
@@ -2,34 +2,34 @@ import mongoose, { Schema } from "mongoose";
 import { IAnime, IUserAnimeEntry } from "./animeTypes";
 
 const animeSchema: Schema = new Schema<IAnime>({
-   malId: { type: Schema.Types.Number, required: true, unique: true },
-   title: { type: Schema.Types.String, required: true, index: true },
-   imageUrl: { type: Schema.Types.String },
-   episodes: { type: Schema.Types.Number },
-   synopsis: { type: Schema.Types.String },
-   url: { type: Schema.Types.String, required: true },
-   genres: [Schema.Types.String],
+    malId: { type: Schema.Types.Number, required: true, unique: true },
+    title: { type: Schema.Types.String, required: true, index: true },
+    imageUrl: { type: Schema.Types.String },
+    episodes: { type: Schema.Types.Number },
+    synopsis: { type: Schema.Types.String },
+    url: { type: Schema.Types.String, required: true },
+    genres: [Schema.Types.String],
 });
 
 const userAnimeEntrySchema: Schema = new Schema<IUserAnimeEntry>(
-   {
-      animeId: { type: Schema.Types.ObjectId, ref: "Anime", index: true },
-      status: {
-         type: String,
-         enum: ["completed", "watching", "planning to watch"],
-         required: true,
-      },
-      finishedDate: { type: Schema.Types.Date },
-      startedDate: { type: Schema.Types.Date },
-      notes: { type: Schema.Types.String },
-      userId: { type: Schema.Types.ObjectId, ref: "User" },
-      rating: { type: Schema.Types.Number },
-   },
-   { timestamps: true }
+    {
+        animeId: { type: Schema.Types.ObjectId, ref: "Anime", index: true },
+        status: {
+            type: String,
+            enum: ["completed", "watching", "planning to watch"],
+            required: true,
+        },
+        finishedDate: { type: Schema.Types.Date },
+        startedDate: { type: Schema.Types.Date },
+        notes: { type: Schema.Types.String },
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        score: { type: Schema.Types.Number },
+    },
+    { timestamps: true }
 );
 
 export const UserAnimeEntry = mongoose.model<IUserAnimeEntry>(
-   "UserAnimeEntry",
-   userAnimeEntrySchema
+    "UserAnimeEntry",
+    userAnimeEntrySchema
 );
 export const Anime = mongoose.model<IAnime>("Anime", animeSchema);
--- src/anime/animeTypes.ts
+++ src/anime/animeTypes.ts
@@ -1,25 +1,31 @@
 import { Types } from "mongoose";
 
 export interface IAnime {
-   malId: Number;
-   title: String;
-   imageUrl?: String;
-   episodes: Number;
-   synopsis?: String;
-   url: String;
-   genres: String[];
+    malId: number;
+    title: string;
+    imageUrl?: string;
+    episodes: number;
+    synopsis?: string;
+    url: string;
+    genres: string[];
+    year: number;
 }
 
 type AnimeWatchStatus = "completed" | "watching" | "planning to watch";
 
 export interface IUserAnimeEntry {
-   animeId: Types.ObjectId;
-   status: AnimeWatchStatus;
-   startedDate?: Date;
-   finishedDate?: Date;
-   notes?: String;
-   userId: Types.ObjectId;
-   createdAt: Date;
-   updatedAt: Date;
-   rating?: Number;
+    animeId: Types.ObjectId;
+    status: AnimeWatchStatus;
+    startedDate?: Date;
+    finishedDate?: Date;
+    notes?: string;
+    userId: Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+    score?: number;
+}
+
+export interface AnimeApiResponse {
+    data: Record<string, any>;
+    pagination?: Record<string, any>;
 }
