33e85e8d3e0d17cacd4781348ca2177b94137c71
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-26 22:45:27 -0500

    Mount getAnime(s) controller functions in the route

--- src/anime/animeRoutes.ts
+++ src/anime/animeRoutes.ts
@@ -1,42 +1,29 @@
+import { Router } from "express";
+import { getAnime, getAnimes } from "./animeController";
+
+const router: Router = Router();
+
 /** 
     get all animes on a certain page (get page 1 if page is not specified) and 
     limit will be set programmatically.
     GET: /api/animes?page=page
 */
+/**
+ * search for an anime from all available animes
+ * GET: /api/animes?search=searchTerm&page=pageNumber (1 if not provided)
+ */
+router.get("/", getAnimes);
 
 /**
  *  get a certain anime info
- *  GET: /api/anime/:id
+ *  GET: /api/animes/:id
  */
-
-/**
- * search for an anime from all available animes
- * GET: /api/anime?search=searchTerm&page=pageNumber (1 if not provided)
- */
+router.get("/:malId", getAnime);
 
 /**
  * get recommendations based on a given anime and view a page of the recommendations (1 if not provided)
- * GET: /api/anime/:id/recommendations?page=page
+ * GET: /api/animes/:id/recommendations?page=page
  */
+router.get("/:malId/recommendations");
 
-/**
- * Add an anime to the user's list. (MUST PICK STATUS, EG. completed, watching, plan-to-watch)
- * POST: /api/anime/:id/add
- * body will include the status and
- * optionally, notes, rating, current episodes watched start date, finished date.
- */
-
-/**
- * view all the animes the user has added.
- * GET: /api/animes/animelist?page=pageNumber (1 if not provided)
- */
-
-/**
- * View animes of certain status
- * GET: /api/animes/animelist?status=status&page=pageNumber (1 if page number isn't provided)
- */
-
-/**
- * search for an anime with a name from the user list
- * GET: /api/animes/animelist?search=searchterm&page=pageNumber (1 if not provided)
- */
+export default router;
