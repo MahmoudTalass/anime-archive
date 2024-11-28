9c4213154c6f9a5e91a00d9c7a4146f40830dc2f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-28 13:42:40 -0500

    Move createJwt to authService since its not a middleware and add unauth checking middleware

--- src/auth/authMiddleware.ts
+++ src/auth/authMiddleware.ts
@@ -2,13 +2,6 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../helpers/errorHelpers";
 
-function createJwt(userId: string, username: string) {
-    const token = jwt.sign({ sub: userId, username }, process.env.JWT_SECRET as string, {
-        expiresIn: "10 days",
-    });
-    return token;
-}
-
 function verifyToken(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers["authorization"];
     const token = authHeader?.split(" ")[1];
@@ -37,4 +30,20 @@ function verifyToken(req: Request, res: Response, next: NextFunction) {
     }
 }
 
-export { verifyToken, createJwt };
+function unauthenticated(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1];
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET as string, (err, payload) => {
+            if (err) {
+                return next();
+            }
+
+            return next(new AppError("You are already logged in", 403));
+        });
+    }
+    return next;
+}
+
+export { verifyToken, unauthenticated };
