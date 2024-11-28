e78bbecb37eac511b01af4429ee9fe9a59088d73
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-27 22:35:51 -0500

    Import and call connectToDb function to connect to db

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import indexRouter from "./indexRoute";
 import dotenv from "dotenv";
 import { errorHandlingMiddleware } from "./helpers/errorHelpers";
 import cors from "cors";
+import connectToDb from "./config/dbConfig";
 
 dotenv.config({ path: __dirname + "/../.env" });
 const app: Express = express();
+connectToDb(process.env.MONGO_URI as string);
 
 app.use(express.json());
 app.use(cors());
@@ -15,4 +17,6 @@ app.use("/api/v1", indexRouter);
 app.use(errorHandlingMiddleware);
 
 const PORT: number = Number(process.env.PORT) || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
