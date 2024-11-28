7fd03e705f7ea0badba15f3ef3f5b629a9258f90
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-27 19:05:11 -0500

    Add cors and json (to parse incoming json) middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express, { Request, Response, Express, NextFunction } from "express";
 import indexRouter from "./indexRoute";
 import dotenv from "dotenv";
 import { errorHandlingMiddleware } from "./helpers/errorHelpers";
+import cors from "cors";
 
 dotenv.config({ path: __dirname + "/../.env" });
 const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", indexRouter);
 
