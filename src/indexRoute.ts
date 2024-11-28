05c10602aa4a8b8eef65589731aface9b1267c1c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-28 14:24:02 -0500

    Mount auth router

--- src/indexRoute.ts
+++ src/indexRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import animesRouter from "./anime/animeRoutes";
+import authRouter from "./auth/authRoutes";
 
 const router: Router = Router();
 
 router.use("/animes", animesRouter);
+router.use("/auth", authRouter);
 
 export default router;
