db8bc116e36079e32e401384068a5e5a351610ab
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-28 13:41:34 -0500

    Validate id if used as identifier and type the function

--- src/user/userService.ts
+++ src/user/userService.ts
@@ -1,21 +1,39 @@
+import { HydratedDocument, isValidObjectId } from "mongoose";
 import { User } from "../user/userModel";
+import { AppError, logError } from "../helpers/errorHelpers";
+import { IUser } from "./userTypes";
 
 class UserService {
-    private static instance: UserService | null = null;
-    private constructor() {}
-
-    static getInstance() {
-        if (UserService.instance === null) {
-            UserService.instance = new UserService();
+    async getUser(
+        identifierValue: string,
+        identifierType: "email" | "id" | "username"
+    ): Promise<HydratedDocument<IUser>> {
+        if (identifierType === "id") {
+            if (!isValidObjectId(identifierValue)) {
+                logError(
+                    400,
+                    `the object id ${identifierValue} provided is an invalid object id`,
+                    "getUser"
+                );
+                throw new AppError("Invalid user id.", 400);
+            }
         }
-        return this.instance;
-    }
 
-    async getUser(identifierValue: string, identifierType: "email" | "id" | "username") {
-        const user = await User.findOne({ [identifierType]: identifierValue });
+        const user: HydratedDocument<IUser> | null = await User.findOne({
+            [identifierType]: identifierValue,
+        });
+
+        if (user === null) {
+            logError(
+                404,
+                `user with identifier ${identifierType} ${identifierValue} was not found`,
+                "getUser"
+            );
+            throw new AppError("User not found.", 404);
+        }
 
         return user;
     }
 }
 
-export default UserService.getInstance();
+export default new UserService();
