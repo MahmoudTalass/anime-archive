d995dd4972586565a3e05f6dda5bb85f42bf360f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-29 15:44:47 -0500

    Create function to return user's anime list and function to get total animes with certain status

--- src/user/userService.ts
+++ src/user/userService.ts
@@ -2,6 +2,8 @@ import { HydratedDocument, isValidObjectId } from "mongoose";
 import { User } from "../user/userModel";
 import { AppError, logError } from "../helpers/errorHelpers";
 import { IUser } from "./userTypes";
+import { AnimeWatchStatus, IUserAnimeEntry } from "../anime/animeTypes";
+import { UserAnimeEntry } from "../anime/animeModels";
 
 class UserService {
     async getUser(
@@ -33,6 +35,46 @@ class UserService {
         }
 
         return user;
+    }
+
+    async getUserAnimes(
+        userId: string,
+        pageNumber: number,
+        perPage: number,
+        searchTerm?: string,
+        status?: AnimeWatchStatus
+    ): Promise<HydratedDocument<IUserAnimeEntry>[]> {
+        if (!isValidObjectId(userId)) {
+            logError(
+                400,
+                `the user id ${userId} provided is an invalid object id`,
+                "getUserAnimeList"
+            );
+            throw new AppError("Invalid user id.", 400);
+        }
+
+        pageNumber -= 1;
+        const animesPerPage = 40;
+
+        let dbQuery: Record<string, any> = { _id: userId };
+        if (status) {
+            dbQuery.status = status;
+        }
+        if (searchTerm) {
+            dbQuery.searchTerm = searchTerm;
+        }
+
+        let animes: HydratedDocument<IUserAnimeEntry>[] = await UserAnimeEntry.find(dbQuery)
+            .sort({ startedDate: "desc" })
+            .skip(pageNumber * animesPerPage)
+            .limit(animesPerPage)
+            .exec();
+
+        return animes;
+    }
+
+    async getTotalAnimes(status?: AnimeWatchStatus): Promise<number> {
+        return await UserAnimeEntry.countDocuments({ status });
     }
 }
 
