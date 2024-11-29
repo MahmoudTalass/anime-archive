6d1221365041b28dee7c2f28f88438440d3169f0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-29 17:34:27 -0500

    Create controller to add anime to user list

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,10 @@
 import { default as asyncHandler } from "express-async-handler";
 import { NextFunction, Request, RequestHandler, Response } from "express";
-import { AppError, logError } from "../helpers/errorHelpers";
 import userService from "./userService";
-import { AnimeWatchStatus } from "../anime/animeTypes";
+import { AnimeWatchStatus, IAnime } from "../anime/animeTypes";
 import { verifyToken } from "../auth/authMiddleware";
+import animeService from "../anime/animeService";
+import { HydratedDocument } from "mongoose";
 
 const getUserAnimeEntries: RequestHandler[] = [
     verifyToken,
@@ -36,10 +37,18 @@ const getUserAnimeEntries: RequestHandler[] = [
     }),
 ];
 
-const addAnimeToUserList: RequestHandler[] = [
+const addAnimeEntryToUserList: RequestHandler[] = [
+    verifyToken,
     asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
         const { malId } = req.body;
+
+        // this retrieval checks if the anime exists and retrieves it. If it doesn't exist,
+        // a 404 error is thrown
+        const anime: HydratedDocument<IAnime> = await animeService.getAnime(malId);
+
+        await userService.addAnimeEntryToUserList(anime.malId.toString(), req.user?.id as string);
+        res.sendStatus(201);
     }),
 ];
 
-export { getUserAnimeEntries };
+export { getUserAnimeEntries, addAnimeEntryToUserList };
