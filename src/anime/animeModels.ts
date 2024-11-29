7d9f0d49a42c33a28d1a166520b8a5d1074f25c4
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-11-29 17:35:01 -0500

    Add default values to optional fields in the user anime entry schema

--- src/anime/animeModels.ts
+++ src/anime/animeModels.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import { IAnime, IUserAnimeEntry } from "./animeTypes";
 
 const animeSchema: Schema = new Schema<IAnime>({
-    malId: { type: Schema.Types.Number, required: true, unique: true },
+    malId: { type: Schema.Types.Number, required: true, unique: true, index: true },
     title: { type: Schema.Types.String, required: true, index: true },
     imageUrl: { type: Schema.Types.String },
     episodes: { type: Schema.Types.Number },
@@ -13,18 +13,18 @@ const animeSchema: Schema = new Schema<IAnime>({
 
 const userAnimeEntrySchema: Schema = new Schema<IUserAnimeEntry>(
     {
-        malId: { type: Schema.Types.ObjectId, ref: "Anime", index: true },
+        malId: { type: Schema.Types.Number, index: true },
         status: {
             type: String,
             enum: ["completed", "watching", "planning to watch"],
             default: "watching",
             required: true,
         },
-        finishedDate: { type: Schema.Types.Date },
-        startedDate: { type: Schema.Types.Date },
-        notes: { type: Schema.Types.String },
+        finishedDate: { type: Schema.Types.Date, default: null },
+        startedDate: { type: Schema.Types.Date, default: null },
+        notes: { type: Schema.Types.String, default: "" },
         userId: { type: Schema.Types.ObjectId, ref: "User" },
-        score: { type: Schema.Types.Number },
+        score: { type: Schema.Types.Number, default: null },
     },
     { timestamps: true }
 );
