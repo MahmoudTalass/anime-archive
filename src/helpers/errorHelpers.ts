950e29ff2b5ebc6191dec45942362cced6a21eec
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 14:26:01 -0500

    Place error response in error object

--- src/helpers/errorHelpers.ts
+++ src/helpers/errorHelpers.ts
@@ -1,33 +1,39 @@
 import { Response, Request, NextFunction } from "express";
 
 export class AppError extends Error {
-    public statusCode: number;
+  public statusCode: number;
 
-    constructor(msg: string, statusCode?: number) {
-        super(msg);
-        this.statusCode = statusCode || 500;
-    }
+  constructor(msg: string, statusCode?: number) {
+    super(msg);
+    this.statusCode = statusCode || 500;
+  }
 }
 
-export function logError(statusCode: number, errorMessage: string, functionName?: string) {
-    let message: string = `status: ${statusCode}, message: ${errorMessage}`;
-    if (functionName) {
-        message += `, occured in ${functionName}`;
-    }
-    console.error(message);
+export function logError(
+  statusCode: number,
+  errorMessage: string,
+  functionName?: string
+) {
+  let message: string = `status: ${statusCode}, message: ${errorMessage}`;
+  if (functionName) {
+    message += `, occured in ${functionName}`;
+  }
+  console.error(message);
 }
 
 export function errorHandlingMiddleware(
-    error: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): void {
-    let statusCode: number = error instanceof AppError ? error.statusCode : 500;
-    const message: string = error.message || "Something went wrong.";
+  let statusCode: number = error instanceof AppError ? error.statusCode : 500;
+  const message: string = error.message || "Something went wrong.";
 
-    res.status(statusCode).json({
-        statusCode,
-        message,
-    });
+  res.status(statusCode).json({
+    error: {
+      statusCode,
+      message,
+    },
+  });
 }
