3858a6f3723eb7f52d4fb06b778aa103fd7e62c0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-14 13:31:59 -0500

    Get the anime year from a different more reliable field from the api data and include the year in the Anime mongoose schema
    Including the year in the mongoose schema fixed the issue where year did not exist as a field in the result

--- src/anime/animeModels.ts
+++ src/anime/animeModels.ts
@@ -2,35 +2,41 @@ import mongoose, { Schema } from "mongoose";
 import { IAnime, IUserAnimeEntry } from "./animeTypes";
 
 const animeSchema: Schema = new Schema<IAnime>({
-    malId: { type: Schema.Types.Number, required: true, unique: true, index: true },
-    title: { type: Schema.Types.String, required: true, index: true },
-    imageUrl: { type: Schema.Types.String },
-    episodes: { type: Schema.Types.Number },
-    synopsis: { type: Schema.Types.String },
-    url: { type: Schema.Types.String, required: true },
-    genres: [Schema.Types.String],
+  malId: {
+    type: Schema.Types.Number,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  title: { type: Schema.Types.String, required: true, index: true },
+  imageUrl: { type: Schema.Types.String },
+  episodes: { type: Schema.Types.Number },
+  synopsis: { type: Schema.Types.String },
+  url: { type: Schema.Types.String, required: true },
+  genres: [Schema.Types.String],
+  year: { type: Schema.Types.Number },
 });
 
 const userAnimeEntrySchema: Schema = new Schema<IUserAnimeEntry>(
-    {
-        malId: { type: Schema.Types.Number, index: true },
-        status: {
-            type: String,
-            enum: ["completed", "watching", "planning to watch"],
-            default: "watching",
-            required: true,
-        },
-        finishedDate: { type: Schema.Types.Date, default: null },
-        startedDate: { type: Schema.Types.Date, default: null },
-        notes: { type: Schema.Types.String, default: "" },
-        userId: { type: Schema.Types.ObjectId, ref: "User" },
-        score: { type: Schema.Types.Number, default: null },
+  {
+    malId: { type: Schema.Types.Number, index: true },
+    status: {
+      type: String,
+      enum: ["completed", "watching", "planning to watch"],
+      default: "watching",
+      required: true,
     },
-    { timestamps: true }
+    finishedDate: { type: Schema.Types.Date, default: null },
+    startedDate: { type: Schema.Types.Date, default: null },
+    notes: { type: Schema.Types.String, default: "" },
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    score: { type: Schema.Types.Number, default: null },
+  },
+  { timestamps: true }
 );
 
 export const UserAnimeEntry = mongoose.model<IUserAnimeEntry>(
-    "UserAnimeEntry",
-    userAnimeEntrySchema
+  "UserAnimeEntry",
+  userAnimeEntrySchema
 );
 export const Anime = mongoose.model<IAnime>("Anime", animeSchema);
--- src/anime/animeTypes.ts
+++ src/anime/animeTypes.ts
@@ -1,83 +1,89 @@
 import { Types } from "mongoose";
 
 export interface IAnime {
-    malId: number;
-    title: string;
-    imageUrl?: string;
-    episodes: number;
-    synopsis?: string;
-    url: string;
-    genres: string[];
-    year: number;
+  malId: number;
+  title: string;
+  imageUrl?: string;
+  episodes: number;
+  synopsis?: string;
+  url: string;
+  genres: string[];
+  year: number;
 }
 
 export type AnimeWatchStatus = "completed" | "watching" | "planning to watch";
 
 export interface IUserAnimeEntry {
-    malId: number;
-    status: AnimeWatchStatus;
-    startedDate?: Date;
-    finishedDate?: Date;
-    notes?: string;
-    userId: Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
-    score?: number;
+  malId: number;
+  status: AnimeWatchStatus;
+  startedDate?: Date;
+  finishedDate?: Date;
+  notes?: string;
+  userId: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  score?: number;
 }
 
 export interface PaginationResponse {
-    perPage: number;
-    page: number;
-    total: number;
-    totalPages: number;
+  perPage: number;
+  page: number;
+  total: number;
+  totalPages: number;
 }
 
 export interface APIPaginationResponse {
-    last_visible_page: number;
-    has_next_page: boolean;
-    current_page: number;
-    items: {
-        count: number;
-        total: number;
-        per_page: number;
-    };
+  last_visible_page: number;
+  has_next_page: boolean;
+  current_page: number;
+  items: {
+    count: number;
+    total: number;
+    per_page: number;
+  };
 }
 
 // represents two top-level properties in the json response from the jikan api when
 // animes are requested
 export interface AnimesApiResponse {
-    data: SingleAnimeApiResponse[];
-    pagination: APIPaginationResponse;
+  data: SingleAnimeApiResponse[];
+  pagination: APIPaginationResponse;
 }
 
 // represents an anime entry thats in the json response from the jikan api
 export interface AnimeEntryApiResponse {
-    mal_id: number;
-    url: string;
-    images: {
-        jpg: {
-            image_url: string;
-            small_image_url: string;
-            large_image_url: string;
-        };
-        webp: {
-            image_url: string;
-            small_image_url: string;
-            large_image_url: string;
-        };
+  mal_id: number;
+  url: string;
+  images: {
+    jpg: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
     };
-    title: string;
+    webp: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+  };
+  title: string;
 }
 
 // represents the properties used from a single anime's information thats
 // retrieved from the jikan api
 export interface SingleAnimeApiResponse extends AnimeEntryApiResponse {
-    title_english: string;
-    episodes: number;
-    synopsis: string;
-    year: number;
-    genres: {
-        name: string;
-    }[];
-    [key: string]: any;
+  title_english: string;
+  episodes: number;
+  synopsis: string;
+  aired: {
+    prop: {
+      from: {
+        year: number;
+      };
+    };
+  };
+  genres: {
+    name: string;
+  }[];
+  [key: string]: any;
 }
